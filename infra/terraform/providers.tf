0d000f2358aa6f0087e1a60d8a3b9443174525df
sinha9t1 <sinha.viki@yahoo.com>
2025-09-29 16:58:45 +0530

    Locked provider version in providers.tf.

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.0"  #this is locking provider version to 5.x versions only
     }
   }
   required_version = ">= 1.13.1, < 1.13.3"
